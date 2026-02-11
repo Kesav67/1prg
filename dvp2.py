a8fced5f8fe70a6db2f6489622d9309a4073aab8
Kesav67 <1by23cs168@bmsit.in>
2026-02-11 09:42:09 +0530

    Add files via upload
    67

---
+++ dvp2.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = {
+    'Department':['CSE','ECE','ME','CE','CV'],
+    'Students':[98,75,60,50,81]
+}
+
+df = pd.DataFrame(data)
+
+plt.pie(df['data'], autopct = '%1.1f%', startangle = 120, color=['orange','lightgreen','red','blue','yellow'])
+plt.title("Number of students in department",fontsize=14)
+plt.label(df['Department'],color='black',fontsize=9)
+plt.show()
